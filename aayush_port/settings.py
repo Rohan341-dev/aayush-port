3d8f7a3566731c2a9ce8dd7f161b16a6d9720725
rohan Yadav <r6461719@gmail.com>
2026-05-16 13:18:34 +0545

    feat: configure media files handling for local storage and static serve URL routing

--- aayush_port/settings.py
+++ aayush_port/settings.py
@@ -94,3 +94,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files (Uploaded images)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
